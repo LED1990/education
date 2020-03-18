144408fc4e7b34ed127fb5d60491e3e990d331a9
Artur Pietrucha <artur.pietrucha@nexio.pl>
2020-03-18 13:12:09 +0100

    develop - cleanup

--- SpringBootApp/src/main/java/app/MainApp.java
+++ SpringBootApp/src/main/java/app/MainApp.java
@@ -9,8 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * @EntityScan needed because entities are in other modules (not in sub packages)
  * @EnableJpaRepositories needed because repo classes are in other module (not subpackage)
  */
-@SpringBootApplication (scanBasePackages = {"db", "model", "app"})
-@EnableJpaRepositories (basePackages = {"db"})
+@SpringBootApplication(scanBasePackages = {"db", "model", "app"})
+@EnableJpaRepositories(basePackages = {"db"})
 @EntityScan(basePackages = {"model"})
 public class MainApp {
     public static void main(String[] args) {
--- database/src/main/java/db/springData/EvaluationDao.java
+++ database/src/main/java/db/springData/EvaluationDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EvaluationDao extends JpaRepository<Evaluation, Long>{
+public interface EvaluationDao extends JpaRepository<Evaluation, Long> {
 }
