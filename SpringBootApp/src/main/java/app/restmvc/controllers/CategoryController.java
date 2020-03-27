a37713a77dfe8fe72b237412924201660a9bae88
Artur Pietrucha <artur.pietrucha@nexio.pl>
2020-03-27 15:31:50 +0100

    guru-restmvc added mapstruct and simple example with Category class

---
+++ SpringBootApp/src/main/java/app/restmvc/controllers/CategoryController.java
@@ -0,0 +1,35 @@
+package app.restmvc.controllers;
+
+import app.restmvc.dto.CategoryDto;
+import app.restmvc.dto.CategoryListDto;
+import app.restmvc.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/categories")
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<CategoryListDto> getAllCategories() {
+        return new ResponseEntity<>(new CategoryListDto(categoryService.getAllCategories()), HttpStatus.OK);
+    }
+
+    @GetMapping("{name}")
+    public ResponseEntity<CategoryDto> getByName(@PathVariable String name){
+        return new ResponseEntity<>(categoryService.getByName(name), HttpStatus.OK);
+    }
+
+}
